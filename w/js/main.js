e9e5aedaa2a09a61f2b3c79882fbf40947f5e148
roshanravi <c.pixels@gmail.com>
2017-05-11 17:05:38 +0530

    VA interaction
    click and expand VA

--- w/js/main.js
+++ w/js/main.js
@@ -21,4 +21,18 @@ $(document).ready(function() {
 	$('.chat-field input').focusout(function(event) {
 		$(this).parents('.assistant-ui.infocus').removeClass('infocus');
 	});
+
+	$('.va-trigger').click(function(e) {
+		e.preventDefault();
+
+		if ($('body').hasClass('va-opened')) {
+			$('body').removeClass('va-opened');
+			$('.assistant-ui').addClass('close');
+			$('section.right-panel').removeClass('col-7').addClass('col-10-half');
+		} else {
+			$('body').addClass('va-opened');
+			$('.assistant-ui').removeClass('close');
+			$('section.right-panel').removeClass('col-10-half').addClass('col-7');
+		}
+	});
 });
